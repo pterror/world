bafc90d47016ee46addc487d137ed46486b4f142
pterror <pterrorbird@gmail.com>
2024-06-03 01:28:03 +1000

    add repl

---
+++ TODO.md
@@ -0,0 +1,5 @@
+### eventual goals
+
+- regular backups to a different file
+  - needs api support
+- regular backups to git
--- text_game.lua
+++ text_game.lua
@@ -15,10 +15,57 @@ mod.function_ = function(source)
 	return setmetatable({ source = source, function_ = loadstring("return " .. source)() }, function_metatable)
 end
 
-local ctx = function(path)
+local replacements = { ["\r"] = "\\r", ["\n"] = "\\n", ["\t"] = "\\t", ["\""] = "\\\"" }
+
+--[[@param s string]]
+local escape = function(s) return s:gsub("[\r\n\t\"]", replacements) end
+
+--[[@param k string|number]]
+local as_key = function(k)
+	if type(k) ~= "string" then return "[" .. k .. "]" end
+	local escaped = escape(k)
+	if escaped == k and not k:find("%s") then return k else return "[\"" .. escaped .. "\"]" end
+end
+
+local write_object
+write_object = function(object, write, on_write_table) --[[@param object table]]
+	on_write_table = on_write_table or function(v) write_object(v, write) end
+	write("{ ")
+	local length = #object
+	for k, v in pairs(object) do
+		if type(k) ~= "number" or k > length then write(as_key(k), " = ") end
+		if type(v) == "string" then
+			write("\"", escape(v), "\"")
+		elseif is_function(v) then
+			write("ctx.function_(\"", escape(v.source), "\")")
+		elseif type(v) ~= "table" then
+			write(tostring(v))
+		else
+			on_write_table(v)
+		end
+		write(", ")
+	end
+	write("}")
+end
+
+local pretty_print = function(val, write)
+	write = write or function(...) io.stdout:write(...) end
+	if type(val) ~= "table" then
+		write(tostring(val))
+	else
+		write_object(val, write)
+	end
+end
+
+local make_ctx = function(path)
 	local obj = {}
 	obj.function_ = mod.function_
 	obj.save = function() mod.save(path, obj.game) end
+	obj.new_object = function(val)
+		val = val or {}
+		obj.game.objects[#obj.game.objects + 1] = val or {}
+		return val
+	end
 	return obj
 end
 
@@ -29,105 +76,136 @@ mod.load = function(path)
 		contents = file:read("*all")
 		file:close()
 	end
-	return loadstring(contents)()(ctx(path))
-end
-
-local replacements = { ["\r"] = "\\r", ["\n"] = "\\n", ["\t"] = "\\t", ["\""] = "\\\"" }
-
---[[@param s string]]
-local escape = function(s) return s:gsub("[\r\n\t\"]", replacements) end
-
---[[@param k string|number]]
-local as_key = function(k)
-	if type(k) ~= "string" then return "[" .. k .. "]" end
-	local escaped = escape(k)
-	if escaped == k then return k else return "[\"" .. escaped .. "\"]" end
+	return loadstring(contents)()(make_ctx(path))
 end
 
 --[[@param path string]]
 --[[@param game text_game]]
 mod.save = function(path, game)
 	local file = assert(io.open(path, "w"))
+	local write = function(...) file:write(...) end
 	local nested_object_lookup = {} --[[@type table<table, integer>]]
 	local nested_objects = {} --[[@type table[] ]]
+	local nested_object_count = 0
 	local process_object
 	process_object = function(object) --[[@param object table]]
 		for _, v in pairs(object) do
 			if type(v) == "table" and not is_function(v) then
-				if not nested_object_lookup[v] then
-					local i = #nested_objects + 1
-					nested_objects[i] = v
-					nested_object_lookup[v] = i
-					process_object(v)
-				end
+				local old_i = nested_object_lookup[v]
+				if old_i then nested_objects[old_i] = nil end
+				nested_object_count = nested_object_count + 1
+				local i = nested_object_count
+				nested_objects[i] = v
+				nested_object_lookup[v] = i
+				process_object(v)
 			end
 		end
 	end
 	for _, object in ipairs(game.objects) do process_object(object) end
-	local nested_end = #nested_objects + 1
-
-	local write_object = function(object) --[[@param object table]]
-		file:write("{ ")
-		local length = #object
-		for k, v in pairs(object) do
-			if type(k) ~= "number" or k > length then file:write(as_key(k), " = ") end
-			if type(v) == "string" then
-				file:write("\"", escape(v), "\"")
-			elseif is_function(v) then
-				file:write("ctx.function_(\"", escape(v.source), "\")")
-			elseif type(v) ~= "table" then
-				file:write(tostring(v))
-			else
-				local rev_i = nested_end - nested_object_lookup[v]
-				file:write("nested[", rev_i, "]")
+	do --[[remove nils from nested_objects]]
+		local i = 1
+		for j = 1, nested_object_count do
+			if nested_objects[j] then
+				if i ~= j then
+					nested_objects[i] = nested_objects[j]
+					nested_objects[j] = nil
+				end
+				i = i + 1
 			end
-			file:write(", ")
 		end
-		file:write("}\n")
+	end
+	local nested_end = #nested_objects + 1
+	nested_object_lookup = {} --[[update indices based on compacted array]]
+	for i, v in ipairs(nested_objects) do nested_object_lookup[v] = i end
+
+	local on_write_table = function(v)
+		local rev_i = nested_end - nested_object_lookup[v]
+		write("nested[", rev_i, "]")
 	end
 
-	file:write("return function(ctx)\n")
-	file:write("\tlocal nested = {}\n")
-	file:write("\tlocal objects = {}\n")
-	file:write("\tlocal game = { objects = objects, }\n")
-	file:write("\tctx.game = game\n")
+	write("return function(ctx)\n")
+	write("\tlocal nested = {}\n")
+	write("\tlocal objects = {}\n")
+	write("\tlocal game = { objects = objects, }\n")
+	write("\tctx.game = game\n")
 	for i = 1, #nested_objects do
 		local rev_i = nested_end - i
-		file:write("\tnested[", i, "] = ")
-		write_object(nested_objects[rev_i])
+		write("\tnested[", i, "] = ")
+		write_object(nested_objects[rev_i], write, on_write_table)
+		write("\n")
 		i = i + 1
 	end
 	for i, object in ipairs(game.objects) do
-		file:write("\tobjects[", i, "] = ")
-		write_object(object)
+		write("\tobjects[", i, "] = ")
+		write_object(object, write, on_write_table)
+		write("\n")
 	end
-	file:write("\treturn ctx\n")
-	file:write("end\n")
+	write("\treturn ctx\n")
+	write("end\n")
 	file:close()
 end
+
+mod.new_game = function() return { objects = {} } end
+
+local normalize_command = { x = "exit", h = "help" }
 
 if pcall(debug.getlocal, 4, 1) then
 	return mod
 else
-	local filename = arg[1]
-	if false then
-		local x = { c = 1, d = "owo", e = false }
-		local game = {
-			objects = { {
-				a = 1,
-				b = { x, x },
-				c = mod.function_("function (_, foo) return foo + 1 end"),
-				d = {},
-				e = mod.function_(
-					"function (ctx) ctx.game.objects[2] = { 3, 4 }; ctx.save(); ctx.game.objects[3] = { 3, true }; ctx.save() end"),
-			} }
-		}
-		print("should be 2:", game.objects[1].c(nil, 1))
-		mod.save("test.lua", game)
-		local ctx2 = mod.load("test.lua")
-		local game2 = ctx2.game
-		print("should be 2:", game2.objects[1].c(nil, 1))
-		game2.objects[1].e(ctx2)
+	local path = arg[1]
+	local success, ctx = pcall(mod.load, path)
+	if not success then
+		local game = mod.new_game()
+		mod.save(path, game)
+		success, ctx = pcall(mod.load, path)
 	end
-	--[[FIXME: run main]]
+	for k, v in pairs(ctx) do _G[k] = v end
+	for k, v in pairs(ctx.game) do _G[k] = v end
+	while true do
+		io.stdout:write("text_game> ")
+		local input = tostring(io.stdin:read("line"))
+		if input:byte(1) == 46 --[[.]] then
+			input = input:sub(2)
+			input = normalize_command[input] or input
+			if input == "exit" then
+				break
+			elseif input == "help" then
+				print([[
+commands: x exit h help
+lua context:
+game - full game state
+  game.objects - list of objects
+object - game.objects
+save() - saves game to the path from which it was loaded
+new_object(val = {}) - add a new object to the list of objects]])
+			else
+				io.stderr:write("error: unknown command\n")
+			end
+		end
+		if input:byte(1) == 59 --[[;]] then
+			input = input:sub(2)
+		else
+			input = "return " .. input
+		end
+		local chunk, err = loadstring(input)
+		if not chunk then
+			io.stderr:write("error: invalid code: ", err, "\n")
+		else
+			local success2, ret = pcall(chunk)
+			if not success2 then
+				io.stderr:write("error: panic running code: ", ret, "\n")
+			elseif ret ~= nil then
+				pretty_print(ret)
+				io.stdout:write("\n")
+			end
+		end
+	end
 end
+
+--[[FIXME:
+printf 'save()\n.exit' | rlwrap luajit text_game.lua games/pterror.lua
+
+this command causes the repl to enter an infinite loop
+exiting on empty input is far from an ideal solution
+]]
+--[[TODO: lazy proxy around objects so they can reference other objects?]]
